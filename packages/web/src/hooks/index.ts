293378954f42d04eefef24e0c2b19380ee81891f
Dāvis Pazars <davisp@mikrotik.com>
2026-02-05 09:22:45 +0200

    chore(web): remove unused useActivities and useDeckLayers hooks

--- packages/web/src/hooks/index.ts
+++ packages/web/src/hooks/index.ts
@@ -1,5 +1,1 @@
-export { useDeckLayers } from './useDeckLayers';
-export type { UseDeckLayersReturn } from './useDeckLayers';
-export { useActivities } from './useActivities';
-export type { UseActivitiesReturn } from './useActivities';
 export { useReducedMotion } from './useReducedMotion';
